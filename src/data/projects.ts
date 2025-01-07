3fc631fd08ae3fd43a4ec4229f8ea75af4875971
katiuska.franco <kati.franco@gmail.com>
2025-01-07 01:04:21 -0500

    Fix url

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,37 +4,37 @@ export const projects = [
     {
       title: "Sitio Web SPA",
       description: "Sitio Web Responsivo para un SPA, con formulario de contacto, desarrollado con HTML y CSS, diseño atractivo y funcional.",
-      image: "src/assets/imagenSPA.png", // Cambia por la imagen de tu proyecto
+      image: "/src/assets/imagenSPA.png", // Cambia por la imagen de tu proyecto
       link: "https://katitafranco.github.io/web_spa_responsive/index.html", // Enlace al repositorio
     },
     {
       title: "Sitio Web Turismo",
       description: "Aplicación web responsiva para explorar lugares turísticos, utilizando HTML, CSS y SASS.",
-      image: "src/assets/imagenTurismo.png", // Cambia por la imagen de tu proyecto
+      image: "/src/assets/imagenTurismo.png", // Cambia por la imagen de tu proyecto
       link: "https://katitafranco.github.io/dynamic-travel-sass/dist/index.html", // Enlace al repositorio
     },
     {
       title: "Agencia de Negocios",
       description: "Web responsiva para agencia de negocios con HTML, CSS, JS, animaciones y video.",
-      image: "src/assets/imagenBusiness.png", // Cambia por la imagen de tu proyecto
+      image: "/src/assets/imagenBusiness.png", // Cambia por la imagen de tu proyecto
       link: "https://katitafranco.github.io/biz-agency-site/", // Enlace al repositorio
     },
     {
       title: "Cotizador de Hosting",
       description: "Layout responsivo de un cotizador de hosting usando Flexbox.",
-      image: "src/assets/imagenCotizador.png", // Cambia por la imagen de tu proyecto
+      image: "/src/assets/imagenCotizador.png", // Cambia por la imagen de tu proyecto
       link: "https://katitafranco.github.io/flex-cotizador-hosting/", // Enlace al repositorio
     },
     {
       title: "Testimonios Interactivos",
       description: "Página web que presenta un slider de testimonios interactivo, utilizando HTML, CSS y JavaScript nativo, con transiciones suaves.",
-      image: "src/assets/imagenSliders.png", // Cambia por la imagen de tu proyecto
+      image: "/src/assets/imagenSliders.png", // Cambia por la imagen de tu proyecto
       link: "https://katitafranco.github.io/slider-testimonios-js/", // Enlace al repositorio
     },
     {
       title: "MERN User CRUD",
       description: "Aplicación CRUD de usuarios usando MongoDB, Mongoose, Express, React, Node.js con animaciones de AOS y notificaciones con SweetAlert2.",
-      image: "src/assets/imagenCRUD2.png", // Cambia por la imagen de tu proyecto
+      image: "/src/assets/imagenCRUD2.png", // Cambia por la imagen de tu proyecto
       link: "https://github.com/katitafranco/crud-mern-stack/tree/master", // Enlace al repositorio
     },
   ];
